3abaf12ecd2800d6d701f5fe408cce8644bfd5d3
Carlo Dee <carlodee.official@proton.me>
2023-04-02 23:46:03 +0800

    Update lsm_params.h

--- kernel-headers/sound/lsm_params.h
+++ kernel-headers/sound/lsm_params.h
@@ -90,7 +90,7 @@ struct snd_lsm_det_event_type {
   __u32 mode;
 };
 struct snd_lsm_sound_model_v2 {
-  __u8 __user *data;
+  __u8 __user * data
   __u8 *confidence_level;
   __u32 data_size;
   enum lsm_detection_mode detection_mode;
@@ -125,7 +125,7 @@ struct snd_lsm_event_status_v3 {
   __u8 payload[0];
 };
 struct snd_lsm_detection_params {
-  __u8 *conf_level;
+  __u8 * conf_level;
   enum lsm_detection_mode detect_mode;
   __u8 num_confidence_levels;
   _Bool detect_failure;
@@ -135,21 +135,21 @@ struct lsm_params_info {
   __u32 module_id;
   __u32 param_id;
   __u32 param_size;
-  __u8 __user *param_data;
+  __u8 __user * param_data
   __u32 param_type;
 };
 struct lsm_params_info_v2 {
   __u32 module_id;
   __u32 param_id;
   __u32 param_size;
-  __u8 __user *param_data;
+  __u8 __user * param_data
   __u32 param_type;
   __u16 instance_id;
   __u16 stage_idx;
   __u32 model_id;
 };
 struct snd_lsm_module_params {
-  __u8 __user *params;
+  __u8 __user * params
   __u32 num_params;
   __u32 data_size;
 };
